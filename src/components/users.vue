ae4723ce38c1036334625177d71a008ebef80deb
xiuers <1174009254@qq.com>
2019-03-15 01:24:53 +0800

    完成用户列表编辑功能

--- src/components/users.vue
+++ src/components/users.vue
@@ -43,12 +43,26 @@
         slot-scope属性是vue提供的一个作用域插槽，
         scope是当前数据别名，指令数据开头必须要保持跟它一致-->
         <template slot-scope="scope">
-          <el-switch v-model="scope.row.mg_state" @change="changeStatus(scope.row)" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+          <el-switch
+            v-model="scope.row.mg_state"
+            @change="changeStatus(scope.row)"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+          ></el-switch>
         </template>
       </el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button type="primary" icon="el-icon-edit" size="mini" plain></el-button>
+          <!-- 因为编辑用户对话框在点击按钮之后就要立马获取数据渲染页面，
+          所以弹出对话框和调用接口渲染页面就要几乎同时运行，
+          就不能单单的弹出对话框，只有调用封装好的方法才能同时完成任务 -->
+          <el-button
+            type="primary"
+            @click="updateOne(scope.row)"
+            icon="el-icon-edit"
+            size="mini"
+            plain
+          ></el-button>
           <el-button
             type="danger"
             @click="delOne(scope.row)"
@@ -67,7 +81,7 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="400"
     ></el-pagination>
-    <!-- 对话框 -->
+    <!-- 对话框 添加用户 -->
     <el-dialog title="添加用户" :visible.sync="addFormVisiable">
       <el-form
         :model="addFormData"
@@ -97,12 +111,39 @@
         <el-button type="primary" @click="submitForm('addForm')">确 定</el-button>
       </div>
     </el-dialog>
+    <!-- 对话框 编辑用户 -->
+    <el-dialog title="编辑用户" :visible.sync="updateFormVisiable">
+      <el-form
+        :model="updateFormData"
+        :rules="rules"
+        label-position="right"
+        label-width="80px"
+        class="my-form"
+        status-icon
+        ref="updateForm"
+      >
+        <!-- status-icon 输入框验证结果反馈的图标 -->
+        <el-form-item label="用户名" prop="username">
+          <el-input disabled v-model="updateFormData.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="updateFormData.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="mobile">
+          <el-input v-model="updateFormData.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="updateFormVisiable = false">取 消</el-button>
+        <el-button type="primary" @click="submitForm('updateForm')">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
 export default {
   data() {
-    // 自定义的校验规则
+    // 自定义的邮箱校验规则
     var checkEmail = (rule, value, callback) => {
       // 延迟验证
       setTimeout(() => {
@@ -118,6 +159,7 @@ export default {
         }
       }, 1000);
     };
+    // 自定义的电话校验规则
     var checkMobile = (rule, value, callback) => {
       // 延迟验证
       setTimeout(() => {
@@ -144,10 +186,18 @@ export default {
       tableData: [],
       // 标记新增用户框是否显示
       addFormVisiable: false,
+      // 标记编辑用户框是否显示
+      updateFormVisiable: false,
       // 新增用户的表单数据
       addFormData: {
         username: "",
         password: "",
+        email: "",
+        mobile: ""
+      },
+      // 编辑用户的表单数据
+      updateFormData: {
+        username: "",
         email: "",
         mobile: ""
       },
@@ -173,6 +223,7 @@ export default {
       let res = await this.$http.get("users", { params: this.sendData });
       // 因为请求拦截器已经设置统一添加token，所以这里不用添加token
       // console.log(res);
+      // 把获取到的数据赋值给用户列表的表单数据
       this.tableData = res.data.data.users;
     },
     // 新增用户的表单验证
@@ -182,13 +233,23 @@ export default {
         // 数据正确
         if (valid) {
           // 提交数据
-          let res = await this.$http.post("users", this.addFormData, {});
+          // 提升res声明
+          let res;
+          if (formName == "addForm") {
+            res = await this.$http.post("users", this.addFormData);
+          } else if (formName == "updateForm") {
+            // 因为在updateOne()方法中已经把获取到的所有数据赋值给了updateFormData,
+            // 所以这里可以直接点出this.updateFormData.id
+            res = await this.$http.put(`users/${this.updateFormData.id}`,this.updateFormData);
+          }
           // 如果数据返回成功了
-          if (res.data.meta.status == "201") {
+          if (res.data.meta.status == "201" || res.data.meta.status == "200") {
             // 再重新获取数据
             this.search();
-            // 然后收起弹框
+            // 然后收起添加用户弹框
             this.addFormVisiable = false;
+            // 然后收起编辑用户弹框
+            this.updateFormVisiable = false;
           }
         } else {
           // 数据错误
@@ -196,6 +257,20 @@ export default {
           return false;
         }
       });
+    },
+    // 用户列表switch改变状态
+    async changeStatus(item) {
+      // 调用接口
+      let res = await this.$http.put(`users/${item.id}/state/${item.mg_state}`);
+    },
+    // 用户列表的编辑数据
+    async updateOne(item) {
+      // 弹框
+      this.updateFormVisiable = true;
+      // 调用接口获取数据
+      let res = await this.$http.get(`users/${item.id}`);
+      // 把获取到的数据赋值给编辑的表单数据
+      this.updateFormData = res.data.data;
     },
     // 用户列表的删除数据
     delOne(item) {
@@ -221,11 +296,6 @@ export default {
             message: "(｡･㉨･｡)ﾉ♡ 爱你么么哒"
           });
         });
-    },
-    // 用户列表switch改变状态
-    async changeStatus(item){
-      // 调用接口
-      let res = await this.$http.put(`users/${item.id}/state/${item.mg_state}`);
     }
   },
   // 获取用户列表的数据
